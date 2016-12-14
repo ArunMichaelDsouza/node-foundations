a19cde5783ec123345baa6ea3382e9c8880675b4
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-12-14 23:22:41 +0530

    Read file sync/async implementation

---
+++ scripts/readFileSyncAsync.js
@@ -0,0 +1,26 @@
+// Read file synchronous implementation
+
+var fs = require('fs'),
+	path = require('path');
+
+var filePath = path.join(__dirname, '../files/file.txt'); // Required as relative paths wont work
+
+/* Alternate method to find file path 
+
+	var filePath = process.cwd() + '/files/file.txt'; 
+
+	process.cwd() gives root path to current working directory
+*/
+
+var contentsSync = fs.readFileSync(filePath, 'UTF-8'); // Provide encoding param for proper content rendering
+console.log('Synchronous request result -');
+console.log(contentsSync);
+
+
+
+// Read file asynchronous implementation
+
+fs.readFile(filePath, 'UTF-8', function(err, contents) {
+	console.log('\nAsynchronous request result -')
+	console.log(contents);
+});

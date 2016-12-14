7c96562230811df7fd2f37d22d8ff953b73d1741
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-12-14 23:51:19 +0530

    Simple Node HTTP server with async file fetching implementation

--- scripts/simpleNodeServer.js
+++ scripts/simpleNodeServer.js
@@ -21,8 +21,7 @@ var fs = require('fs'),
 var server = http.createServer(function(req, res) {
 	fs.readFile(filePath, function(err, contents) {
 		res.writeHead(200, {'content-type': 'text/html'});
-		res.write(contents);
-		res.end();
+		res.end(contents);
 	});	
 });
 

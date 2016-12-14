48d875591893cf005a45bcafa91ba101880d6393
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-12-15 01:37:00 +0530

    Node server response creation with 'request' event

--- scripts/simpleNodeServer.js
+++ scripts/simpleNodeServer.js
@@ -17,12 +17,25 @@ var fs = require('fs'),
 	filePath = process.cwd() + '/files/file.txt';
 
 // Create HTTP server and return headers and text
-var server = http.createServer(function(req, res) {
+// var server = http.createServer(function(req, res) {
+// 	fs.readFile(filePath, function(err, contents) {
+// 		res.writeHead(200, {'content-type': 'text/html'});
+// 		res.write(contents);
+// 		res.end();
+// 	});	
+// });
+
+
+
+// Node server response creation with 'request' event
+ var server = http.createServer();
+
+ server.on('request', function(req, res) {
 	fs.readFile(filePath, function(err, contents) {
 		res.writeHead(200, {'content-type': 'text/html'});
 		res.write(contents);
 		res.end();
-	});	
+	});
 });
 
 

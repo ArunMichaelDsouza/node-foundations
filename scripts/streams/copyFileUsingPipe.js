d08cd19e5487c39120090b11bb0a801d8ec94a61
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-12-17 22:04:32 +0530

    Copy file contents using pipe - creating read and write streams

---
+++ scripts/streams/copyFileUsingPipe.js
@@ -0,0 +1,14 @@
+// Copy file contents using pipe
+
+var fs = require('fs'),
+	path = require('path');
+
+var file1 = path.join(__dirname, '../../files/file.txt'),
+	file2 = path.join(__dirname, '../../files/new-file.txt');
+
+// Create read and write streams
+var readStream = fs.createReadStream(file1), 
+	writeStream = fs.createWriteStream(file2);
+
+// Pipe contents from read to write stream
+readStream.pipe(writeStream);
--- scripts/streams/dataStreamUsingPipe.js
+++ scripts/streams/dataStreamUsingPipe.js
@@ -4,7 +4,7 @@ var http = require('http');
 
 http.createServer(function(req, res) {
 	res.writeHead(200);
-	req.pipe(res); // Pipe request data back to response
+	req.pipe(res); // Pipe request data back to response (pipes data from a readable stream to a writable stream)
 }).listen(8000);
 
 console.log('Server running...');
